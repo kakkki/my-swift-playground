a125c785f09959883dc79218dbcfbb7ff77f0851
kakkki <kakki4062@gmail.com>
2022-02-06 16:19:54 +0900

    Not needs  typealis because of Swift Type Inference on Compile

--- MyPlayground.playground/Sources/ProtocolOriented-Programming/GenericProtocolSample.swift
+++ MyPlayground.playground/Sources/ProtocolOriented-Programming/GenericProtocolSample.swift
@@ -24,6 +24,10 @@ struct Mouse {
 }
 
 struct Cat: AnimalP {
+    // @see https://docs.swift.org/swift-book/LanguageGuide/Generics.html
+    // Thanks to Swift’s type inference, you don’t actually need to declare a concrete Item of Int as part of the definition of IntStack. Because IntStack conforms to all of the requirements of the Container protocol, Swift can infer the appropriate Item to use, simply by looking at the type of the append(_:) method’s item parameter and the return type of the subscript. Indeed, if you delete the typealias Item = Int line from the code above, everything still works, because it’s clear what type should be used for Item.
+    // ↓の一行がなくてもFoodType = Mouseが自明なので動作する。
+    // Swiftがコンパイル時に型推論してるから
     typealias FoodType = Mouse
     func eat(food: Mouse) {
         print("yum")
